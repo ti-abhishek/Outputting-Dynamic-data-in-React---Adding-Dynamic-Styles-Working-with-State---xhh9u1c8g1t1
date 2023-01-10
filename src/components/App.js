63b47f524e8f9b05331b2a7d6f2b9ef259d4d584
ti-abhishek <ti.abhishek15@gmail.com>
2023-01-10 21:58:00 +0530

    n

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [name, setName] = useState('');
+  const [currData, setCurrData] = useState('____');
+
+  function handleOnchange(event) {
+    setName(event.target.value);
+  }
+  function handleOnClick(){
+    setCurrData(name);
+  }
 //code here
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' value={name} onChange={handleOnchange} ></input>
+      <button id='button' onClick={handleOnClick}>Click</button>
+      <p id='text'> Hello my name is {currData} and I study at Newton School</p>
     </div>
   )
 }
